c32d3be15ba921548c71f2527bdb509e88f29ad1
dawsonkaybwsc <159972020+dawsonkaybwsc@users.noreply.github.com>
2025-09-04 09:51:01 +1200

    player movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,21 @@
 extends CharacterBody2D
 
-const SPEED = 500.0
+const SPEED = 750.0
+const TURN_SPEED := 10.0
 
 func _physics_process(delta: float) -> void:
 	
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	if direction !=Vector2.ZERO:
+		velocity = direction.normalized() * SPEED
+		
+		#this adds rotation to the spaceship btw
+		var target_rotation = direction.angle() + PI / 2
+		
+		rotation = lerp_angle(rotation, target_rotation, TURN_SPEED * delta)
+	else:
+		velocity = Vector2.ZERO
 
 	
 	velocity = direction * SPEED
