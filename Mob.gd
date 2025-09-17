2665caa409f1c07356ffbb569dc9a69d861cd013
dawsonkaybwsc <159972020+dawsonkaybwsc@users.noreply.github.com>
2025-09-18 10:20:17 +1200

    gf

---
+++ Mob.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
+	$AnimatedSprite2D.animation = mob_types.pick_random()
+	$AnimatedSprite2D.play()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+@warning_ignore("unused_parameter")
+func _process(delta):
+	pass
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
--- scenes/background.gd
+++
@@ -1,8 +0,0 @@
-extends AnimatedSprite2D
-
-
-
-func _ready():
-	pass # Replace with function body.
-func _process(delta):
-	pass
