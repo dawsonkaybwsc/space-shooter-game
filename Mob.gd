c2fe3c4998b5c015826532a0aaed47d091466e00
dawsonkaybwsc <159972020+dawsonkaybwsc@users.noreply.github.com>
2025-10-07 13:42:56 +1300

    enemy

--- Mob.gd
+++ Mob.gd
@@ -1,18 +1,28 @@
-extends RigidBody2D
+extends CharacterBody2D
 
+@export var speed: float = 120.0 
+var player: CharacterBody2D = null
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	# Animation setup is fine
 	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
 	$AnimatedSprite2D.animation = mob_types.pick_random()
 	$AnimatedSprite2D.play()
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-@warning_ignore("unused_parameter")
-func _process(delta):
-	pass
+func _physics_process(delta):
+	if player == null:
+		player = get_tree().root.get_node_or_null("GameWorld/Player")
+		
+	
+	if is_instance_valid(player):
+		var direction: Vector2 = (player.global_position - global_position).normalized()
+		velocity = direction * speed
+	else:
+		velocity = Vector2.ZERO 
+		
+	move_and_slide() 
+
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
