ee14911244ebd9da7bfc8e0096c1f2fed9921a0d
dawsonkaybwsc <159972020+dawsonkaybwsc@users.noreply.github.com>
2025-10-07 14:08:17 +1300

    enemyv2

--- Mob.gd
+++ Mob.gd
@@ -1,28 +1,25 @@
 extends CharacterBody2D
 
-@export var speed: float = 120.0 
+@export var speed: float = 350.0 
+var direction: int = 1
+
 var player: CharacterBody2D = null
 
 func _ready():
-	# Animation setup is fine
 	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
 	$AnimatedSprite2D.animation = mob_types.pick_random()
 	$AnimatedSprite2D.play()
-
-
-func _physics_process(delta):
-	if player == null:
-		player = get_tree().root.get_node_or_null("GameWorld/Player")
-		
 	
-	if is_instance_valid(player):
-		var direction: Vector2 = (player.global_position - global_position).normalized()
-		velocity = direction * speed
-	else:
-		velocity = Vector2.ZERO 
-		
-	move_and_slide() 
+func _physics_process(delta):
 
+	velocity.x = speed * direction
+	move_and_slide()
+	if is_on_wall():
+		direction *= -1
+		if direction == -1:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
