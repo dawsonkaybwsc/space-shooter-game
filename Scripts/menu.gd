7e27edf5b2bd340013a8e13f03bfeaced2cd4bf0
dawsonkaybwsc <159972020+dawsonkaybwsc@users.noreply.github.com>
2025-10-20 11:56:55 +1300

    menu

---
+++ Scripts/menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+# This function is connected to your "Start" button's 'pressed' signal
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
